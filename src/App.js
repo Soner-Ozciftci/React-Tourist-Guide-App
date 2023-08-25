cf6505951840d9ce75d026aec0d2366216ef04f7
Soner-Ozciftci <ozciftcisoner@gmail.com>
2023-08-25 12:16:31 +0200

    holiday selection

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
-import "./App.css"
+import "./App.css";
 import React, { useState } from "react";
-import Categories from ' ./Categories';
+import Categories from "./Categories";
 import items from './Data';
 import Menu from './Menu';
 
-function App() {
-
 const allCategories = ["all", ...new Set(items.map((item)=> item.category))];
 
-const [menuItems,setMenuItems] = useState(items);
-const [activeCategory,setActiveCategory] =useState("");
-const [categories,setCategories] = useState(allCategories);
+function App(){
+
+const [menuItems, setMenuItems] = useState(items);
+const [activeCategory, setActiveCategory] =useState("");
+const [categories, setCategories] = useState(allCategories);
+
+const filterItems = (category) => {
+
+}
 
   return (
     <main>
-
+<section className="holiday section">
+<div className="title">
+  <h2>Cukurova Swimming Places</h2>
+  <div className="underline"></div>
+   </div>
+   <Categories categories={categories} activeCategory={activeCategory} filterItems={filterItems}  />
+</section>
     </main>
   );
 }
