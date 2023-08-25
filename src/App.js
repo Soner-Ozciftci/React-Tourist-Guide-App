08e76cdc3246e1183ad9cc2c10fde25ab10543c0
Soner-Ozciftci <ozciftcisoner@gmail.com>
2023-08-25 12:00:02 +0200

    activeCategory

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./App.css"
 import React, { useState } from "react";
-
+import Categories from ' ./Categories';
+import items from './Data';
+import Menu from './Menu';
 
 function App() {
 
-  
+const allCategories = ["all", ...new Set(items.map((item)=> item.category))];
+
+const [menuItems,setMenuItems] = useState(items);
+const [activeCategory,setActiveCategory] =useState("");
+const [categories,setCategories] = useState(allCategories);
+
   return (
     <main>
 
